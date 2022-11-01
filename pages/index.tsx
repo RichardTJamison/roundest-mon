3eb0f3c6fedaa5b63e39c3bc3e1581fe12140efb
RichardTJamison <richardtjamison99@gmail.com>
2022-11-01 10:18:44 -0400

    Published

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,16 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="text-2xl text-red-200">Hello World</div>
+    <div className="h-screen w-screen flex flex-col justify-center">
+      <div className='text-2xl text-center'>
+        Which Pokemon is Rounder?
+      </div>
+      <div className='p-2'></div>
+      <div className='border rounded p-8 flex justify-between item-center max-w-2xl'>
+        <div className='w-16 h-16 bg-red-200'/>
+        <div className='p-8'>Vs</div>
+        <div className='w-16 h-16 bg-red-200'/>
+      </div>
+    </div>
   )
 }
