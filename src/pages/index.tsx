7bfea6bb611939d3a613e9917accb3ed0db0882c
RichardTJamison <richardtjamison99@gmail.com>
2022-11-01 12:23:44 -0400

    added pokemon generator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon'
 import { trpc } from '@/utils/trpc'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const {data} =trpc.hello.useQuery({text:"Ricky"})
-  if (!data){return <div>Loading</div>}
-  if(data){return<div>{data.greeting}</div>}
+  const [first,second] = getOptionsForVote()
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className='text-2xl text-center'>
@@ -14,9 +13,9 @@ export default function Home() {
       </div>
       <div className='p-2'></div>
       <div className='border rounded p-8 flex justify-between item-center max-w-2xl'>
-        <div className='w-16 h-16 bg-red-200'/>
+        <div className='w-16 h-16 bg-red-800'>{first}</div>
         <div className='p-8'>Vs</div>
-        <div className='w-16 h-16 bg-red-200'/>
+        <div className='w-16 h-16 bg-red-800'>{second}</div>
       </div>
     </div>
   )
