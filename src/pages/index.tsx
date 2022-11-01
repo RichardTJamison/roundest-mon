af091c929c76f5adb69c1e957b95ec4810c339ce
RichardTJamison <richardtjamison99@gmail.com>
2022-11-01 11:32:03 -0400

    Added TRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import { trpc } from '@/utils/trpc'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const {data} =trpc.hello.useQuery({text:"Ricky"})
+  if (!data){return <div>Loading</div>}
+  if(data){return<div>{data.greeting}</div>}
   return (
-    <div className="h-screen w-screen flex flex-col justify-center">
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className='text-2xl text-center'>
         Which Pokemon is Rounder?
       </div>
